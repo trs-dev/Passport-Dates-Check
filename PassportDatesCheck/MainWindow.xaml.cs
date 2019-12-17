da799252efc20370e9d26bc616cdacd7c7cedb4d
trs-dev <taras2017ua@gmail.com>
2019-12-17 17:03:53 +0200

    Добавьте файлы проекта.

---
+++ PassportDatesCheck/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassportDatesCheck
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+
+            this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width+6;
+            this.Top = System.Windows.SystemParameters.PrimaryScreenHeight - this.Height-32;
+        }
+
+
+        private void OnlyNumber_Input(object sender, TextCompositionEventArgs e)
+        {
+            TextBox ctrl = sender as TextBox;
+            e.Handled = "0123456789".IndexOf(e.Text) < 0;//only digits
+        }
+
+        private void NoSpaces_Input(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true; // don`t allow to imput space
+            }
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
---
+++ PassportDatesCheck/MainWindowViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassportDatesCheck
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        private string _DateSecondPart = "";
+        public string DateSecondPart
+        {
+            get { return _DateSecondPart; }
+            set
+            {
+                if (value == _DateSecondPart) return;
+                _DateSecondPart = value;
+                OnPropertyChanged();
+                OnPropertyChanged("DateFirstPart");
+                OnPropertyChanged("Turns");
+            }
+        }
+
+        public string DateFirstPart
+        {
+            get
+            {
+                if (Int32.TryParse(DateSecondPart, out int dateSecondPart) &&
+                    DateSecondPart.Length == 2 &&
+                    dateSecondPart <= DateTime.Now.Year % 100)
+                {
+                    return "20";
+                }
+                return "19";
+            }
+        }
+
+        public int DateFull
+        {
+            get
+            {
+                if (Int32.TryParse(DateFirstPart, out int dateFirstPart) &&
+                    DateSecondPart.Length == 2 &&
+                    Int32.TryParse(DateSecondPart, out int dateSecondPart))
+                    return dateFirstPart * 100 + dateSecondPart;
+
+                return 0;
+            }
+        }
+
+
+        public class turns_view_model
+        {
+            public string Description { get; set; }
+            public string Year { get; set; }
+            public string FontWeihht { get; set; }
+        }
+        public int[] ShowedDates = new int[] { 12, 14, 16, 18, 25, 45 }; //All dates we must to show
+
+        public ObservableCollection<turns_view_model> Turns
+        {
+            get
+            {
+                var rezult = new ObservableCollection<turns_view_model>();
+                int date = DateFull;
+                int _year_last_value = DateTime.Now.Year+100;
+
+                foreach (var d in ShowedDates.Reverse())
+                {
+                    string _description = Convert.ToInt32(d) + " р. - ";
+                    string _year = "";
+                    string _fontWeihht = "Normal";
+
+                    int y = 0;
+
+                    if (date != 0)
+                    {
+                        y = date + Convert.ToInt32(d);
+                        _year = y.ToString();
+                    }
+
+                    if (_year_last_value > DateTime.Now.Year &&
+                        y <= DateTime.Now.Year)
+                        _fontWeihht = "Bold";
+
+                    _year_last_value = y;
+                    rezult.Insert(0, new turns_view_model { Description = _description, Year = _year, FontWeihht = _fontWeihht });
+                }
+                return rezult;
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
